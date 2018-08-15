517148d7cdf90b122dfdf3bdfeadafae093a1d3f
J Michael Thurman <jmt@jmichaelthurman.com>
2018-08-15 18:46:46 -0500

    added fields to form and POST handling, body parsing to server.js

--- survey-form/server.js
+++ survey-form/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
 let app = express();
 
 const port = process.env.PORT || 3000;
 
+let urlencodedParser = bodyParser.urlencoded({extended: false});
+
+let jsonParser = bodyParser.json();
 
 app.use((req,res, next) =>{
     var now = new Date().toString();
@@ -23,9 +27,13 @@ app.use(express.static(__dirname + '/public'));
 
 // Route handlers
 
-app.get('/', (req,res) => {
-    res.render('index.html');
-})
+app.post('/', urlencodedParser, function (req,res) {
+    res.send({
+      status:'OK',
+      status_code: '200'
+    })
+    console.log(req.body);
+    });
 
 app.get('/bad',(req,res)=>{
     res.send({
